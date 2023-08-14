257109f90b00ed0a12db937dc86c8929b142635c
Alicia Miedl <alicia@aliciabytes.com>
2023-08-14 18:00:54 +0200

    Add robots.txt file
    We want to allow almost all crawlers to access all the content on the site.
    But we want to block GPTBot from accessing it.
    
    Resolves #24

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,51 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
 import rehypePrettyCode from 'rehype-pretty-code';
-
 import frappe from '/utils/shiki-themes/frappe.json';
 import latte from '/utils/shiki-themes/latte.json';
 import macchiato from '/utils/shiki-themes/macchiato.json';
 import mocha from '/utils/shiki-themes/mocha.json';
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://aliciabytes.com",
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    robotsTxt({
+      host: "aliciabytes.com",
+      policy: [
+        {
+          userAgent: 'GPTBot',
+          disallow: '/'
+        },
+        {
+          userAgent: '*',
+          allow: '/'
+        }
+      ],
+      sitemap: false,
+    })
+  ],
   markdown: {
     remarkPlugins: [],
-    rehypePlugins: [
-      [rehypePrettyCode, {
-        theme: {
-          frappe: frappe,
-          latte: latte,
-          macchiato: macchiato,
-          mocha: mocha,
-        }
-      }]
-    ],
-    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, {
+      theme: {
+        frappe: frappe,
+        latte: latte,
+        macchiato: macchiato,
+        mocha: mocha
+      }
+    }]],
+    syntaxHighlight: false
   },
   vite: {
     build: {
       assetsInlineLimit: -1
     },
     ssr: {
-      noExternal: [
-        'sanitize.css'
-      ]
+      noExternal: ['sanitize.css']
     }
   }
 });
