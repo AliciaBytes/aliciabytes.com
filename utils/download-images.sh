3ce78608d08350f3db2a37820e8a6f25ae856bf3
Alicia Miedl <alicia@aliciabytes.com>
2023-08-15 20:49:41 +0200

    Add 404 page

--- utils/download-images.sh
+++ utils/download-images.sh
@@ -7,4 +7,4 @@ curl -o public/icon-192.png 'https://ik.imagekit.io/cc2j8aae5c/favicon/icon-192_
 curl -o public/icon-512.png 'https://ik.imagekit.io/cc2j8aae5c/favicon/icon-512_5WlYQ5-ii.png'
 curl -o public/apple-touch-icon.png 'https://ik.imagekit.io/cc2j8aae5c/favicon/apple-touch-icon_pD28D1th_.png'
 curl -o public/opengraph.png 'https://ik.imagekit.io/cc2j8aae5c/opengraph/AliciaBytes%20-%20OpenGraph%20image_0ntE8ssE8.png'
-# curl -o public/404.svg 'https://ik.imagekit.io/cc2j8aae5c/images/page_not_found_uiKZQvIXA.svg'
+curl -o public/404.svg 'https://ik.imagekit.io/cc2j8aae5c/images/page_not_found_uiKZQvIXA.svg'
