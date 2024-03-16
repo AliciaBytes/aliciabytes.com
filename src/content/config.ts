f579414c4d8ff475cf51c215a50e405149054ee5
Alicia Miedl <alicia@aliciabytes.com>
2024-03-16 09:58:35 +0100

    Add categories for pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ const page = defineCollection({
         published: z.date(),
         lastUpdated: z.date().optional(),
         tags: z.array(z.string()),
+        categories: z.array(z.string()).optional(),
         relatedPages: z.array(reference('page')),
         backlinks: z.array(reference('page')),
     })
