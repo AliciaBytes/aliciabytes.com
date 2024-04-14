4564ff7abff6d5ae1a4df102bd71ecba76e3cd70
Alicia Miedl <alicia@aliciabytes.com>
2024-04-14 10:55:31 +0200

    Add my gpg key to my website

--- src/utils/remark.ts
+++ src/utils/remark.ts
@@ -15,67 +15,67 @@ import macchiato from '/utils/shiki-themes/macchiato.json';
 import mocha from '/utils/shiki-themes/mocha.json';
 
 function getOptions(theme?: null | "frappe" | "latte" | "macchiato" | "mocha") {
-    let rehypePrettyCodeOptions: { grid: boolean, theme: any | null } = {
-        grid: false,
-        theme: null,
-    }
+  let rehypePrettyCodeOptions: { grid: boolean, theme: any | null } = {
+    grid: false,
+    theme: null,
+  }
 
-    switch (theme) {
-        case undefined:
-            rehypePrettyCodeOptions.theme = {
-                frappe,
-                latte,
-                macchiato,
-                mocha,
-            }
-            break;
-        case null:
-            break;
-        case "frappe":
-            rehypePrettyCodeOptions.theme = {
-                frappe,
-            }
-            break;
-        case "latte":
-            rehypePrettyCodeOptions.theme = {
-                latte,
-            }
-            break;
-        case "macchiato":
-            rehypePrettyCodeOptions.theme = {
-                macchiato,
-            }
-            break;
-        case "mocha":
-            rehypePrettyCodeOptions.theme = {
-                mocha,
-            }
-            break;
-    }
+  switch (theme) {
+    case undefined:
+      rehypePrettyCodeOptions.theme = {
+        frappe,
+        latte,
+        macchiato,
+        mocha,
+      }
+      break;
+    case null:
+      break;
+    case "frappe":
+      rehypePrettyCodeOptions.theme = {
+        frappe,
+      }
+      break;
+    case "latte":
+      rehypePrettyCodeOptions.theme = {
+        latte,
+      }
+      break;
+    case "macchiato":
+      rehypePrettyCodeOptions.theme = {
+        macchiato,
+      }
+      break;
+    case "mocha":
+      rehypePrettyCodeOptions.theme = {
+        mocha,
+      }
+      break;
+  }
 
-    return rehypePrettyCodeOptions
+  return rehypePrettyCodeOptions
 }
 
 export default async function render_markdown(content: string, theme?: null | "frappe" | "latte" | "macchiato" | "mocha") {
-    const rehypePrettyCodeOptions = getOptions(theme);
-    const result = String(await unified()
-        .use(remarkParse)
-        .use(remarkGfm)
-        .use(remarkHeaderId, {
-            defaults: true
-        })
-        .use(remarkKroki, {
-            server: "https://kroki.io/",
-            output: "inline-svg",
-        })
-        .use(remarkRehype)
-        .use(rehypePrettyCode, rehypePrettyCodeOptions)
-        .use(rehypeStringify)
-        .process(content))
+  const rehypePrettyCodeOptions = getOptions(theme);
+  const result = String(await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkHeaderId, {
+      defaults: true
+    })
+    .use(remarkKroki, {
+      server: "https://kroki.io/",
+      output: "inline-svg",
+    })
+    .use(remarkRehype)
+    .use(rehypePrettyCode, rehypePrettyCodeOptions)
+    .use(rehypeStringify)
+    .process(content))
 
-    if (theme === null) {
-        return sanitizeHtml(result);
-    } else {
-        return result;
-    }
+  if (theme === null) {
+    return sanitizeHtml(result);
+  } else {
+    return result;
+  }
 }
