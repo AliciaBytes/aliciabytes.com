ca798f8e2776e4b637bcb2eeaef45c4b2b31b91b
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2024-10-20 09:23:38 +0200

    Add a new collection for montly notes to my site

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,12 +12,10 @@ backlinks: []
 aliases: []
 ---
 */
-const pages = defineCollection({
-  type: 'content',
-  schema: z.object({
+
+const common_schema = z.object({
     title: z.string(),
     excerpt: z.string(),
-    prefix: z.string().default(""),
     published: z.date(),
     lastUpdated: z.date().optional(),
     tags: z.array(z.string()).optional(),
@@ -32,31 +30,28 @@ const pages = defineCollection({
     })).optional(),
     aliases: z.array(z.string()).optional(),
   })
+
+const pages = defineCollection({
+  schema: common_schema.extend({
+    prefix: z.string().default(''),
+  }),
 });
 
 const notes = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    excerpt: z.string(),
-    prefix: z.string().default("notes/"),
-    published: z.date(),
-    lastUpdated: z.date().optional(),
-    tags: z.array(z.string()).optional(),
-    categories: z.array(z.string()).optional(),
-    relatedPages: z.array(z.object({
-      collection: z.string(),
-      id: z.string(),
-    })).optional(),
-    backlinks: z.array(z.object({
-      collection: z.string(),
-      id: z.string(),
-    })).optional(),
-    aliases: z.array(z.string()).optional(),
-  })
+  schema: common_schema.extend({
+    prefix: z.string().default('notes/'),
+  }),
 });
+
+const monthly_notes = defineCollection({
+  schema: common_schema.extend({
+    prefix: z.string().default('monthly_notes/'),
+  }),
+})
 
 export const collections = {
   pages,
   notes,
+  monthly_notes,
 };
