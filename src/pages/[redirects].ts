c7c2f16eb1e787e7a9d7374a3cd50ca42cc6dfdb
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2025-02-04 17:22:23 +0100

    Fix redirects

--- src/pages/[redirects].ts
+++ src/pages/[redirects].ts
@@ -1,21 +1,27 @@
 import type { APIContext } from "astro";
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 
 export async function getStaticPaths() {
-  return [{ params: { redirects: "_redirects" } }]
+  return [{ params: { redirects: "_redirects" } }];
 }
 
 export async function GET(context: APIContext) {
-
   const pages = [
-    ...await getCollection("pages"),
-    ...await getCollection("notes")
+    ...(await getCollection("pages")),
+    ...(await getCollection("notes")),
+    ...(await getCollection("monthly notes")),
   ];
 
-  return new Response(pages.
-    filter(page => page.data.aliases?.length).
-    flatMap(page => page.data.aliases?.
-      map(alias => ({ source: `${alias}`, destination: `/${page.data.prefix}${page.slug}` }))).
-    map(redirect => `${redirect?.source} ${redirect?.destination} 301`).
-    join("\n"));
+  return new Response(
+    pages
+      .filter((page) => page.data.aliases?.length)
+      .flatMap((page) =>
+        page.data.aliases?.map((alias) => ({
+          source: `${alias}`,
+          destination: `/${page.data.prefix}${page.id}`,
+        }))
+      )
+      .map((redirect) => `${redirect?.source} ${redirect?.destination} 301`)
+      .join("\n")
+  );
 }
