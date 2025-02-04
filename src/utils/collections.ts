dee3554d253b9727823d674f631a04f16bbe4080
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2025-02-04 18:10:18 +0100

    Fix errors

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,2 +1,5 @@
-export const collectionFilter = (entry) =>
-  !entry.id.startsWith("_") && entry?.data?.published;
+import type { CollectionEntry, DataEntryMap } from "astro:content";
+
+export const collectionFilter = (
+  entry: CollectionEntry<"monthly notes" | "notes" | "pages">
+): boolean => !entry.id.startsWith("_") && entry?.data?.published;
